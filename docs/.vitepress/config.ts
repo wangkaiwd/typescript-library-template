a9eb84d6ce38230ccbd75e7c5531a98587595466
wangkaiwd <1162486176@qq.com>
2022-01-16 19:26:26 +0800

    docs: add favorite icon for documentation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   title: 'TypeScript Library Template',
   description: 'Write TypeScript Library without extra ',
   base: '/typescript-library-template/',
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }],
+  ],
   themeConfig: {
     // Type is `DefaultTheme.Config`
     logo: '/logo.png',
