990e1cf4120eac3ceee4941df6c3f713e9258258
wangkaiwd <1162486176@qq.com>
2022-07-31 14:33:41 +0800

    feat: change rollup config 1. replace typescript2 plugin with esbuild plugin 2. add json,commonjs,resolve rollup plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,23 @@
-import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import esbuild from 'rollup-plugin-esbuild';
 import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
 
 const mode = process.env.MODE;
 const isProd = mode === 'prod';
-const pkg = require('./package.json');
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
 
 export default [
   {
     input: `lib/index.ts`,
+    external,
     output: [
       {
         file: pkg.main,
@@ -27,14 +37,21 @@ export default [
         sourcemap: !isProd
       },
     ],
-    plugins: [typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: { compilerOptions: { sourceMap: !isProd } }
-    })],
+    plugins: [
+      resolve(),
+      commonjs(),
+      json({ preferConst: true }),
+      esbuild()
+    ],
   },
   {
-    input: "build/types/index.d.ts",
-    output: [{ file: "build/my-lib.d.ts", format: "es" }],
+    input: 'build/types/index.d.ts',
+    output: [
+      {
+        file: 'build/my-lib.d.ts',
+        format: 'es'
+      }
+    ],
     plugins: [dts()],
   },
 ];
